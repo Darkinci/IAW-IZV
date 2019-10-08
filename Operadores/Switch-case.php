7196f871c986f5c259ae9c7061f520d1dbd740a3
Darkinci <jorge.ortega.nieto@gmail.com>
2019-10-08 11:56:20 +0200

    Actualizado con mas ejercicios de switch y elseif

--- Operadores/Switch-case.php
+++ Operadores/Switch-case.php
@@ -7,8 +7,8 @@
     <link rel="shortcut icon" type="image" href="D:\2º ASIR A\IAW - Ruth Galera\Clase\Icono\ico.png" />
     <title> Operador: Switch con Case </title>
 </head>
-<body style="background-color:#BCEDC3">
-    <h1 style="color:#FFDC00;text-align:center"> EXERCISE: SWITCH & CASE </h1>
+<body style="background-color:#d8ffd5">
+    <h1 style="color:#5cff51;text-align:center"> EXERCISE: SWITCH & CASE </h1>
     <h1 style="color:#FF6C00;text-align:center"> By Jorge Ortega 2019/20 </h1>
     <?php
 
@@ -45,6 +45,59 @@
                 echo "<h3 style='text-align:center;'>No es un color para una fruta conocida! </h3>";
                 break;
         }
+
+        echo "<h2 style='color:#ffd000;text-align:center'> ***************************************</h2>";
+        echo "<h2 style='color:blue;text-align:center'> Guess what day am I?</h2>";
+
+        $dia = 1;
+
+        echo "<h5 style='color:purple;text-align:center'> *********** CON SWITCH ************</h5>";
+
+        switch ($dia){
+            case 1:
+                echo "<h3 style='color:red;text-align:center;'>El dia es LUNES </h3>";
+                break;
+            case 2:
+                echo "<h3 style='color:red;text-align:center;'>El dia es MARTES </h3>";
+                break;
+            case 3:
+                echo "<h3 style='color:red;text-align:center;'>El dia es MIERCOLES </h3>";
+                break;
+            case 4:
+                echo "<h3 style='color:red;text-align:center;'>El dia es JUEVES </h3>";
+                break;
+            case 5:
+                echo "<h3 style='color:red;text-align:center;'>El dia es VIERNES </h3>";
+                break;
+            case 6:
+                echo "<h3 style='color:red;text-align:center;'>El dia es SABADO </h3>";
+                break;
+            case 7:
+                echo "<h3 style='color:red;text-align:center;'>El dia es DOMINGO </h3>";
+                break;
+            default:
+                echo "<h3 style='color:red;text-align:center;'>No es un dia de la semana </h3>";
+                break;
+        }
+
+        echo "<h5 style='color:purple;text-align:center'> *********** CON IF-ELSE ***********</h5>";
+
+        if($dia == 1){
+            echo "<h3 style='color:red;text-align:center;'>El dia es Lunes</h3>";
+        } elseif ($dia == 2){
+                echo "<h3 style='color:red;text-align:center;'>El dia es Martes</h3>";
+            }elseif($dia == 3){
+                    echo "<h3 style='color:red;text-align:center;'>El dia es Miercoles</h3>";
+                }elseif ($dia == 4){
+                        echo "<h3 style='color:red;text-align:center;'>El dia es Jueves</h3>";
+                    }elseif($dia == 5){
+                            echo "<h3 style='color:red;text-align:center;'>El dia es Viernes</h3>";
+                        }elseif($dia == 6){
+                                echo "<h3 style='color:red;text-align:center;'>El dia es Sabado</h3>";
+                            }elseif($dia == 7){
+                                    echo "<h3 style='color:red;text-align:center;'>El dia es Domingo</h3>";
+                                }else echo "<h3 style='color:red;text-align:center;'>Ese numero de dia no es correcto</h3>";  
+
 
     ?>
 </body>
