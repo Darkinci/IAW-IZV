1613c95d26e6f61f7e1e63a0c5f207494f007723
Darkinci <jorge.ortega.nieto@gmail.com>
2020-01-15 14:40:32 +0100

    conseguido un mini avance, pero falla en IDCIUDAD

--- PDO-Bases de datos/monumentos/TurismoLibreria.php
+++ PDO-Bases de datos/monumentos/TurismoLibreria.php
@@ -1,13 +1,13 @@
 <?php
 
-function inserta_ciudad(&$bd, $nomNueCiu){
+function inserta_ciudad(&$bd, $nombreciudad){
 
-    $idciudad = 1;
     // idciudad - nombre - habitantes - pais - monumentos 
-    $sql= "Insert into ciudad (idciudad, nombre) values (:idciudad :nombre)"; // aqui tenemos que poner SOLO el nombre de la ciudad, y el/los nombres de los documentos
+    $sql= "Insert into ciudad ( nombre) values (:nombre)"; // aqui tenemos que poner SOLO el nombre de la ciudad, y el/los nombres de los documentos
     $sentencia=$bd->prepare($sql);
-    $sentencia->bindValue("idciudad" , $idciudad); // Aqui tendriamos que hacer la funcion de asignar un numero, pero que no sea aleatorio
-    $sentencia->bindValue("nombre", $nomNueCiu);
+    $idciudad = 1;
+    //$sentencia->bindValue("idciudad" , $idciudad); // Aqui tendriamos que hacer la funcion de asignar un numero, pero que no sea aleatorio
+    $sentencia->bindValue("nombre", $nombreciudad);
     //$sentencia->bindValue("monumentos" ; $nombreMonumento);
     $sentencia->execute();
 
